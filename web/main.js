43d9ac10d85ef50d2a2c3d1f23158408444a2af3
kasa10 <kirill.kas@mail.ru>
2021-03-20 14:10:56 +0300

    V3.0

--- web/main.js
+++ web/main.js
@@ -14,24 +14,23 @@
 		}
 
 	});
-}());
 
-(function () {
-	const burger = document.querySelector('.burger');
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('radio2');
+	const radio2 = document.querySelector('.radio2');
+	radio2.addEventListener('click', () => {
+		// radio2.classList.toggle('radio2');
 
-		if ( document.querySelector('.burger_bl').style.display=='none')
+		if ( document.querySelector('.txt2').style.display=='none')
 		{
-			document.querySelector('.burger_bl').style.display='block';
-			document.querySelector('.map2').style.width='1000px';
+			document.querySelector('.txt2').style.display='inline-block';
+			document.querySelector('.txt1').style.display='none';
 		}
 		else {
-			document.querySelector('.burger_bl').style.display = 'none';
-			document.querySelector('.map2').style.width = '1300px';
+			document.querySelector('.txt2').style.display = 'none';
+			document.querySelector('.txt1').style.display='inline-block';
 		}
 
 	});
+
 }());
 
 
