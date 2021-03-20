102acfcf8ee04c67a80e32a7ef176755e1d71ca4
kasa10 <kirill.kas@mail.ru>
2021-03-20 13:18:00 +0300

    v2.2

--- web/main.js
+++ web/main.js
@@ -16,6 +16,25 @@
 	});
 }());
 
+(function () {
+	const burger = document.querySelector('.burger');
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('radio2');
+
+		if ( document.querySelector('.burger_bl').style.display=='none')
+		{
+			document.querySelector('.burger_bl').style.display='block';
+			document.querySelector('.map2').style.width='1000px';
+		}
+		else {
+			document.querySelector('.burger_bl').style.display = 'none';
+			document.querySelector('.map2').style.width = '1300px';
+		}
+
+	});
+}());
+
+
 
 
 
